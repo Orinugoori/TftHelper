7835c22f8bb210674cf8c92f17180d79c1a5ffe8
Orinugoori_art <120167509+Orinugoori@users.noreply.github.com>
2025-06-25 03:47:42 +0900

    Remove augment keyword system and processing function

--- app/src/main/java/com/orinugoori/tfthelper/Augment.kt
+++ app/src/main/java/com/orinugoori/tfthelper/Augment.kt
@@ -1,8 +1,5 @@
 package com.orinugoori.tfthelper
 
-import android.util.Log
-import com.orinugoori.tfthelper.data.processor.AugmentProcessor
-
 data class AugmentResponse(
     val data: Map<String, Augment>
 )
@@ -12,21 +9,9 @@ data class Augment(
     val tier : String = "",
     val name: String,
     val image: ImageInfo,
-    val description : String = "",
-    val keyword : List<String> = emptyList()
+    val description : String = ""
 )
 
 data class ImageInfo(
     val full: String
 )
-
-/**
- * 증강 데이터 처리 함수
- * 기존의 거대한 하드코딩된 맵 대신 자동 키워드 추출 시스템 사용
- * 
- * @param rawAugments API에서 받은 원본 증강 데이터
- * @return 키워드와 티어 정보가 추가된 처리된 증강 데이터
- */
-fun processAugments(rawAugments: List<Augment>): List<Augment> {
-    return AugmentProcessor.processAugments(rawAugments)
-}
